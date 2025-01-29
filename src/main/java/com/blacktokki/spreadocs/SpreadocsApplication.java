843c9f17e661af94c7d325c3d694c6a98a24f8d1
blacktokki <39031723+blacktokki@users.noreply.github.com>
2025-01-30 01:46:51 +0900

    Initial commit2

---
+++ src/main/java/com/blacktokki/spreadocs/SpreadocsApplication.java
@@ -0,0 +1,13 @@
+package com.blacktokki.spreadocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpreadocsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpreadocsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blacktokki/spreadocs/SpreadocsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blacktokki.spreadocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpreadocsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
