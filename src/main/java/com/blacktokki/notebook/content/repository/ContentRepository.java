de733232da719104269450d79e5640ccf7b697fc
blacktokki <39031723+blacktokki@users.noreply.github.com>
2025-08-02 13:31:46 +0000

    feat : 편집내역 압축처리

--- src/main/java/com/blacktokki/notebook/content/repository/ContentRepository.java
+++ src/main/java/com/blacktokki/notebook/content/repository/ContentRepository.java
@@ -3,6 +3,7 @@ package com.blacktokki.notebook.content.repository;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Modifying;
@@ -11,6 +12,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.blacktokki.notebook.content.entity.Content;
+import com.blacktokki.notebook.content.entity.ContentType;
 
 @Repository
 public interface ContentRepository extends JpaRepository<Content, Long>, JpaSpecificationExecutor<Content>{
@@ -20,4 +22,6 @@ public interface ContentRepository extends JpaRepository<Content, Long>, JpaSpec
 
     @Query("Select c from Content c Where c.parentId in (:parentIds) order by c.updated desc limit 1")
     Optional<Content> lastContent(@Param("parentIds") List<Long> parentIds);
+
+    List<Content> findByTypeAndParentIdOrderByIdDesc(Pageable pageable, ContentType type, Long parentId);
 }
--- src/main/java/com/blacktokki/notebook/content/service/ContentService.java
+++ src/main/java/com/blacktokki/notebook/content/service/ContentService.java
@@ -5,8 +5,11 @@ import jakarta.persistence.criteria.Predicate;
 import jakarta.persistence.criteria.Root;
 
 import java.time.ZonedDateTime;
+import java.util.LinkedList;
 import java.util.List;
 
+import org.bitbucket.cowwoc.diffmatchpatch.DiffMatchPatch;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
@@ -17,12 +20,16 @@ import com.blacktokki.notebook.content.dto.ContentDto;
 import com.blacktokki.notebook.content.dto.ContentOrderDto;
 import com.blacktokki.notebook.content.dto.ContentQueryParam;
 import com.blacktokki.notebook.content.entity.Content;
+import com.blacktokki.notebook.content.entity.ContentOption;
+import com.blacktokki.notebook.content.entity.ContentType;
 import com.blacktokki.notebook.content.repository.ContentRepository;
 import com.blacktokki.notebook.core.dto.BaseUserDto;
 import com.blacktokki.notebook.core.service.restful.RestfulService;
 
 @Service
 public class ContentService extends RestfulService<ContentDto, Content, Long> {
+    private final DiffMatchPatch dmp = new DiffMatchPatch();
+
     @Override
     public Predicate toPredicate(String key, Object value, Root<Content> root, CriteriaBuilder builder){
         if(key.equals("withDeleted")){
@@ -59,6 +66,41 @@ public class ContentService extends RestfulService<ContentDto, Content, Long> {
     @Override
     public Content toEntity(ContentDto t) {
         return Content.builder().userId(t.userId()).parentId(t.parentId()).type(t.type()).order(t.order()).title(t.title()).description(t.description()).option(t.option()).build();
+    }
+
+    private void createDelta(ContentDto newDomain) {
+        List<Content> lastContents = ((ContentRepository) getRepository()).findByTypeAndParentIdOrderByIdDesc(
+            Pageable.ofSize(2), ContentType.SNAPSHOT, newDomain.parentId());
+        if (lastContents.size() == 2) {
+            Content newer = lastContents.get(0);
+            Content older = lastContents.get(1);
+            LinkedList<DiffMatchPatch.Diff> diffs = dmp.diffMain(older.getDescription(), newer.getDescription());
+            dmp.diffCleanupSemantic(diffs);
+            String delta = dmp.diffToDelta(diffs);
+            if (delta.length() < newer.getDescription().length()) {
+                ContentOption.Map option = new ContentOption.Map();
+                option.put(ContentOption.SNAPSHOT_ID, older.getId());
+                Content deltaContent = Content.builder()
+                    .id(newer.getId())
+                    .userId(newer.getUserId())
+                    .parentId(newer.getParentId())
+                    .type(ContentType.DELTA)
+                    .order(newer.getOrder())
+                    .title(newer.getTitle())
+                    .description(delta)
+                    .option(option).build();
+                getRepository().save(deltaContent);
+            }
+        }
+    }
+
+    @Override
+    @Transactional
+    public ContentDto create(ContentDto newDomain){
+        if (false || newDomain.type().equals(ContentType.SNAPSHOT)) {
+            createDelta(newDomain);
+        }
+        return super.create(newDomain);
     }
 
     @Override
